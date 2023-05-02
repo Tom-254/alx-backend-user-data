3bcb4aa62f82385840acf442eb6e0833c615ecab
Tom-254 <211mseeratiego@gmail.com>
2023-05-02 23:54:37 +0000

    0x00. Personal data

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,18 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Checks if a path requires authentication.
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        for excluded_path in excluded_paths:
+            if path.startswith(excluded_path):
+                if path[len(excluded_path):].startswith('/') or path == excluded_path:
+                    return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Returns authorization header.

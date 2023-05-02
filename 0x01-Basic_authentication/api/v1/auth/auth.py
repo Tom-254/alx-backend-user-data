d24a814734ed0b1652852e0de6d6f399ace50f4a
Tom-254 <211mseeratiego@gmail.com>
2023-05-02 23:59:40 +0000

    0x00. Personal data

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,14 +13,11 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Checks if a path requires authentication.
         """
-        if not path:
-            return True
-        if not excluded_paths:
-            return True
-        for excluded_path in excluded_paths:
-            if path.startswith(excluded_path):
-                if path == excluded_path.rstrip("/") or path.endswith("/"):
-                    return False
+        if path is not None and excluded_paths is not None:
+            for excluded_path in excluded_paths:
+                if path.startswith(excluded_path):
+                    if path == excluded_path.rstrip("/") or path.endswith("/"):
+                        return False
         return True
 
     def authorization_header(self, request=None) -> str:

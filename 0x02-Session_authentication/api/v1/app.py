68def8d2a401159f056d6dd66f3e2e612de09cb8
Tom-254 <211mseeratiego@gmail.com>
2023-05-04 18:58:21 +0000

    0x00. Personal data

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -61,8 +61,10 @@ def authenticate_user():
     ]
     if auth and auth.require_auth(request.path, excluded_paths):
         user = auth.current_user(request)
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None and \
+                auth.session_cookie(request) is None:
             abort(401)
+
         if user is None:
             abort(403)
         request.current_user = user

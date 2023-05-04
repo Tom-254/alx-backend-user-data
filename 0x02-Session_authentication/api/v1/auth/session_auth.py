1e7600165750d21796c2f70ed93bf59cdf531c33
Tom-254 <211mseeratiego@gmail.com>
2023-05-04 18:52:10 +0000

    0x00. Personal data

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -15,9 +15,15 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Creates a session id for the user.
+        """Creates a Session ID for a user_id
         """
         if isinstance(user_id, str):
             session_id = str(uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Returns a User ID based on a Session ID:
+        """
+        if isinstance(session_id, str):
+            return self.user_id_by_session_id.get(session_id)
